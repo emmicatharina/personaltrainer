118e3e4877f3e66d180f6a30248af01c6d748695
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-05-05 13:04:43 +0300

    Change AG Grid to material-table

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,11 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, forwardRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
+import moment from 'moment';
+import MaterialTable from 'material-table';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function Customerlist() {
   const [customers, setCustomers] = useState([]);
+  const [columns, setColumns] = useState([
+    { title: 'First name', field: 'firstname' },
+    { title: 'Last name', field: 'lastname' },
+    { title: 'Email', field: 'email' },
+    { title: 'Phone', field: 'phone' },
+    { title: 'Street', field: 'streetaddress' },
+    { title: 'Post code', field: 'postcode' },
+    { title: 'City', field: 'city' }
+  ])
 
   const fetchCustomers = () => {
     fetch('https://customerrest.herokuapp.com/api/customers')
@@ -18,7 +45,7 @@ function Customerlist() {
     fetchCustomers();
   }, []);
 
-  const columns = [
+  /*const columns = [
     { headerName: 'First name', field: 'firstname', sortable: true, filter: true },
     { headerName: 'Last name', field: 'lastname', sortable: true, filter: true },
     { field: 'email', sortable: true, filter: true },
@@ -26,19 +53,39 @@ function Customerlist() {
     { headerName: 'Street', field: 'streetaddress', sortable: true, filter: true },
     { headerName: 'Post code', field: 'postcode', sortable: true, filter: true },
     { field: 'city', sortable: true, filter: true },
-  ]
+  ]*/
+
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
   return (
-      <div>
-        <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
-            <AgGridReact
-            rowData={customers}
-            columnDefs={columns}
-            pagination={true}
-            paginationPageSize={15}
-            suppressCellSelection={true}
-            />
-        </div>
+      <div style={{ maxWidth: '90%' }}>
+        <MaterialTable 
+          columns={columns}
+          data={customers}
+          title="Customers"
+          icons={tableIcons}
+          editable={{
+            
+          }}
+        />
       </div>
   );
 }
--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -1,11 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, forwardRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
+import moment from 'moment';
+import MaterialTable from 'material-table';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function Traininglist() {
   const [trainings, setTrainings] = useState([]);
+  const [columns, setColumns] = useState([
+    { title: 'Date', field: 'date', render: rowData => moment(trainings.date).format("LLL") },
+    { title: 'Duration', field: 'duration' },
+    { title: 'Activity', field: 'activity' },
+    { title: 'First name', field: 'customer.firstname' },
+    { title: 'Last name', field: 'customer.lastname' }
+  ])
 
   const fetchTrainings = () => {
     fetch('https://customerrest.herokuapp.com/gettrainings')
@@ -18,27 +43,39 @@ function Traininglist() {
     fetchTrainings();
   }, []);
 
-  const columns = [
-    { field: 'date', sortable: true, filter: true },
-    { field: 'duration', sortable: true, filter: true },
-    { field: 'activity', sortable: true, filter: true },
-    { headerName: 'First name', field: 'customer.firstname', sortable: true, filter: true  },
-    { headerName: 'Last name', field: 'customer.lastname', sortable: true, filter: true  }
-  ]
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
 
   return (
-      <div>
-        <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
-            <AgGridReact
-            rowData={trainings}
-            columnDefs={columns}
-            pagination={true}
-            paginationPageSize={15}
-            suppressCellSelection={true}
-            />
-        </div>
-      </div>
+    <div style={{ maxWidth: '90%' }}>
+      <MaterialTable 
+        columns={columns}
+        data={trainings}
+        title="Trainings"
+        icons={tableIcons}
+        editable={{
+          
+        }}
+      />
+  </div>
   );
 }
 
