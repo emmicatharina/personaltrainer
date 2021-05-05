4b0339b7e37b5d7e87780c7640707e61bcc2c042
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-05-05 19:39:37 +0300

    Change tabs to drawer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,164 @@
 import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Customerlist from './components/Customerlist';
 import Traininglist from './components/Traininglist';
-import { AgGridReact } from 'ag-grid-react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Snackbar from '@material-ui/core/Snackbar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 
-import './App.css';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Snackbar,
+  Drawer,
+  Button,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  CssBaseline
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import PeopleIcon from '@material-ui/icons/People';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import EventIcon from '@material-ui/icons/Event';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+
 
 function App() {
-  const [value, setValue] = useState('customers');
-  const handleChange = (event, value) => {
-    setValue(value);
-  }
+  const [value, setValue] = useState('Customers');
+
+  const drawerWidth = 240;
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: drawerWidth,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  }));
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <AppBar position="static">
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}>
         <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6">
             Personal Trainer
           </Typography>
         </Toolbar>
-        <Tabs value={value} onChange={handleChange}>
-          <Tab value="customers" label="Customers" />
-          <Tab value="trainings" label="Trainings" />
-        </Tabs>
       </AppBar>
-      {value === 'customers' && <Customerlist />}
-      {value === 'trainings' && <Traininglist />}
+      <Drawer className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button key={'Customers'} onClick={() => setValue('Customers')}>
+            <ListItemIcon><PeopleIcon /></ListItemIcon>
+            <ListItemText primary={'Customers'} />
+          </ListItem>
+          <ListItem button key={'Trainings'} onClick={() => setValue('Trainings')}>
+            <ListItemIcon><DirectionsRunIcon /></ListItemIcon>
+            <ListItemText primary={'Trainings'} />
+          </ListItem>
+          <Divider />
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        {value === 'Customers' && <Customerlist />}
+        {value === 'Trainings' && <Traininglist />}
+        </main>
     </div>
   );
 }
